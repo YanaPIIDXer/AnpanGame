e3ff604077a754d3e8e6846e53a2651607cab160
YanaP <yana_p_iidxer@yahoo.cojp>
2018-10-30 16:34:01 +0900

    テスト用に書き換えたままコミットしてしまったので修正。

--- Classes/APIURLs.cpp
+++ Classes/APIURLs.cpp
@@ -1,6 +1,6 @@
 #include "APIURLs.h"
 
-const std::string APIURLs::URLBase = "http://2128.220.121.112/~YanaP/AnpanGameAPI/";
+const std::string APIURLs::URLBase = "http://218.220.121.112/~YanaP/AnpanGameAPI/";
 
 const std::string APIURLs::Auth = URLBase + "Auth/Auth.php";
 const std::string APIURLs::ShopData = URLBase + "Shop/ShopData.php";
