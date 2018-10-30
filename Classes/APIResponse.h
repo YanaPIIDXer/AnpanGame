95484c358d18004bd5ccea9e05efb9d722e08b3b
YanaP <yana_p_iidxer@yahoo.cojp>
2018-10-30 20:57:46 +0900

    APIの戻り値をまとめた構造体を定義するためのヘッダファイルを追加。

---
+++ Classes/APIResponse.h
@@ -0,0 +1,14 @@
+#ifndef __APIRESPONSE_H__
+#define __APIRESPONSE_H__
+
+#include <string>
+
+// AuthAPI–ß‚è’l.
+struct AuthResponse
+{
+	std::string Id;
+	int Point;
+	int HighScore;
+};
+
+#endif		// #ifndef __APIRESPONSE_H__
